4a29b60e8bfc66b9b9e323a01568c698f78131a8
Nicol Vishan <nicolvish.ire120@gmail.com>
2022-11-04 15:54:10 +0100

    Initial Commit

---
+++ Fifa Countries/Fifa_CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fifa_CountriesApp.swift
+//  Fifa Countries
+//
+//  Created by Nicol Warnakulasooriya on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Fifa_CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
